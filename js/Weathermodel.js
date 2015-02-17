66b312adeb43be739e01b997e9789137edf277e6
Nicole Cano <arbolkiri@gmail.com>
2015-02-17 01:04:46 -0600

    pushing to gh

--- js/Weathermodel.js
+++ js/Weathermodel.js
@@ -1,9 +1,48 @@
-//INDEX HTML IS LOADING FIRST AND THEN WEATHERTEMPLATE LOADS. NEED TO GET TEMPLATE TO RENDER WHEN I WANT IT TO, NOT AUTOMATICALLY.
+//NOTE: In WeatherTemplate need a for loop or something for this weeks temperatures
+//Do I need more than one view?
+//How to get icons to display?
+//Need search button to work
+//Couldn't get Router to work
+//7daytemplate is not rendering
 
 ;
 (function(exports) { //backbone model handles communication with REST APIs
 
     "use strict";
+    // var GeoRouter = Backbone.Router.extend({//this isn't routing the way I want it to
+    //     routes: {
+    //         '': 'index',
+    //         'dayview/:name': 'dayview',
+    //     },
+    //     events: {
+    //         "click": "extendedforecast",
+    //     },
+    //     extendedforecast: function(event){
+    //         event.preventDefault();
+    //         g.el.innerHTML += "ClickHere!"
+    //         Backbone.trigger("extendedforecast")
+    //         document.querySelector(".container").innerHTML += "ClickHere!"
+    //     },
+    //     index: function(){
+    //         this.navigate('dayview/today',{
+    //             trigger: false,
+    //             replace: true
+    //             });
+    //     },
+    //     dayview: function(name) {
+    //         debugger;
+    //         v.options.view = name;
+    //         v.trigger('changeview');
+
+    //     },
+    //     initialize: function() {
+    //         Backbone.history.start();
+    //         m.geofetch();
+
+    //         // console.log(m.geofetch)
+    //     }
+    // });
+
     Backbone.GeoModel = Backbone.Model.extend({
         geo: function() {
             var x = $.Deferred(),
@@ -39,28 +78,6 @@
                 "?callback=?"
             ].join('')
         }
-        // defaults: {
-        //     forecast: "Cold Front",
-        //     lat: 29.7628,
-        //     lng: 95.3831
-        // },
-        // validate: function(attrs){
-        //     if(attrs.lat === 0 || attrs.lng === 0){
-        //         return "Lat or Lng not set."
-        //     }
-        // }
-        // initialize: function(){
-        //     this.on("change", function(model, options){
-        //         console.log(model)
-        //         console.log(options)
-        //     })
-        //     this.on("change: forecast", function(model, value, options){
-        //         console.log("forecast changed")
-        //     })
-        //     this.on("invalid", function(model, errorMessage, options){
-        //         alert("Error, please try again")
-        //     })
-        // }
     })
 
     Backbone.TemplateView = Backbone.View.extend({
@@ -71,23 +88,25 @@
                 x.resolve(cache[url]);
             } else {
                 $.get(url).then((function(d) {
-
+                    // debugger;
                     this.cache[url] = _.template(d);
                     x.resolve(_.template(d));
                 }).bind(this));
             }
             return x;
         },
-        loadTemplate: function(name) {//HOW DO I GET THE TEMPLATE TO LOAD WHEN I WANT IT TO AND NOT RIGHT AWAY?
+        loadTemplate: function(name) {
             return this.stream('./templates/' + name + '.html');
         },
         initialize: function(options) {
             this.options = options;
+
             this.model && this.model.on("change", this.render.bind(this));
+            // this.on('changeview', this.render, this);
         },
         render: function() {
             var self = this;
-            this.loadTemplate(this.options.view || this.view).then(function(fn) {
+            this.loadTemplate(this.options.view).then(function(fn) {
                 debugger;
                 self.model && (self.el.innerHTML = fn(self.model.toJSON()));
             })
@@ -103,9 +122,19 @@
         el: ".container",
         model: exports.m
     });
+    //  exports.D = new Backbone.TemplateView({
+    //     view: "7DayTemplate",
+    //     el: ".container",
+    //     model: exports.m
+    // });
+    // exports.g = new GeoRouter();
+
+// console.log(g);
 
     m.geofetch().then(function(data) {
         data; // { ... } --> data from network request
-    })
+    // console.log(data);
+    });
+
 
 })(typeof module === "object" ? module.exports : window);
